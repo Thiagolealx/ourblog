90191306ba5d358e4f131636883c9f4d291327cb
thiago.oliveira <thiago.oliveira@codatsa.pb.gov.br>
2025-03-26 13:23:15 -0300

    Initial commit

---
+++ ourside/blog/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from wagtail.models import Orderable, Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel, InlinePanel
+from datetime import date
+from modelcluster.models import ParentalKey,ParentalManyToManyField
+from wagtail.snippets.models import register_snippet
+from django import forms
+from taggit.models import TaggedItemBase
+from modelcluster.tags import ClusterTaggableManager
+
+class BlogIndexPage(Page):
+    description =  RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("description"),
+    ]
+class BlogPostTag(TaggedItemBase):
+    content_object = ParentalKey("BlogPostPage",
+                                    on_delete=models.CASCADE, related_name="tagged_items")
+
+class BlogPostPage(Page):
+    date = models.DateField("Post date", default=date.today)
+    intro = RichTextField(blank=True)
+    body = RichTextField(blank=True)
+    author = ParentalManyToManyField("blog.Author",blank=True)
+    tags = ClusterTaggableManager(through=BlogPostTag, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("date"),
+        FieldPanel("author", widget=forms.CheckboxSelectMultiple),
+        FieldPanel("title"),
+        FieldPanel("body"),
+        InlinePanel("image_gallery", label="Image Gallery"),
+        FieldPanel("tags"),
+    ]
+
+
+class BlogPageImageGallery(Orderable):
+    page = ParentalKey(BlogPostPage, on_delete=models.CASCADE, related_name="image_gallery")
+    image = models.ForeignKey(
+        "wagtailimages.Image", on_delete=models.CASCADE, related_name="+"
+    )
+    caption = models.CharField(max_length=255, blank=True)
+
+@register_snippet
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    author_image = models.ForeignKey(
+        "wagtailimages.Image", on_delete=models.CASCADE, related_name="+"
+    )
+    panels = [
+        FieldPanel("name"),
+        FieldPanel("author_image"),
+    ]
+
+    def __str__(self):
+        return self.name
+
+class TagIndexPage(Page):
+    pass
---
+++ ourside/ourside/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-7ohkagk^p5ca%&spfp)x%sbx5i2i_!st$57owdwr$r+_)_wf-c"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
